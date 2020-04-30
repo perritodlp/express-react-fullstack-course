7cacee8a6c4b7e36d7afb6061720c5720f2fa862
Fernando Rengifo <ferengifo@MacBook-Air-de-Fernando.local>
2020-04-30 02:33:37 -0500

    Front end mockup without styles finished

---
+++ src/app/components/TaskList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { requestTaskCreation } from '../store/mutations';
+import { Link } from 'react-router-dom';
+
+export const TaskList = ({tasks, name, createNewTask, id})=>(
+    <div>
+        <h3>
+            {name}
+        </h3>
+        <div>
+            {tasks.map(task=>(
+                <Link to={`/task/${task.id}`} key={task.id}>
+                    <div>{task.name}</div>
+                </Link>
+            ))}
+        </div>
+        <button onClick={()=>createNewTask(id)}>Add New</button>
+    </div>
+)
+
+const mapStateToProps = (state, ownProps)=>{
+    let groupID = ownProps.id;
+    return {
+        name:ownProps.name,
+        id:groupID,
+        tasks: state.tasks.filter(task=>task.group === groupID)
+    }
+}
+
+const mapDispatchToProps = (dispatch, id)=>{
+    return {
+        createNewTask(id) {
+            console.log("Creating new task...", id);
+            dispatch(requestTaskCreation(id));
+        }
+    }
+}
+
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
